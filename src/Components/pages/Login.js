4622e7f00c7347e735e14e09e691509fed9f9dca
ArielScc <lf0ariel@gmail.com>
2021-03-14 00:37:05 -0400

    Casi implementado JWT

--- src/Components/pages/Login.js
+++ src/Components/pages/Login.js
@@ -1,12 +1,28 @@
+import axios from 'axios';
 import React from 'react';
 
 export const Login = () => {
+  const authentication = e => {
+    e.preventDefault();
+    const form = e.target;
+
+    const data = {
+      email: form.email.value,
+      password: form.password.value,
+    };
+
+    axios
+      .post('https://api-edteam.alejogs4.now.sh/login', data)
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="ed-grid">
       <div className="l-block"></div>
       <div className="m-to-center m-60 lg-30">
         <h2 className="center">Iniciar sesión</h2>
-        <form action="">
+        <form onSubmit={authentication}>
           <div className="form__item">
             <label htmlFor="email">
               Correo electrónico
@@ -14,6 +30,7 @@ export const Login = () => {
                 type="email"
                 id="email"
                 placeholder="ingrese su email"
+                name="email"
                 required
               />
             </label>
@@ -24,6 +41,7 @@ export const Login = () => {
               <input
                 type="password"
                 id="password"
+                name="password"
                 placeholder="ingrese su contraseña"
                 required
               />
